e37d94409208cef9009c854d56e90c017bd970c5
Jerry <aa29003748@gmail.com>
2024-12-11 16:47:22 +0800

    傑威的分支

--- src/test/java/SmartLegalSearch/ReadJsonTest.java
+++ src/test/java/SmartLegalSearch/ReadJsonTest.java
@@ -16,19 +16,20 @@ public class ReadJsonTest {
 	@Test
 	public void readJsonTest() {
 		ReadJson readJson = new ReadJson();
-		ReadJsonVo data = readJson.readJson("C:\\Users\\user\\Desktop\\Jerry\\Java\\臺灣士林地方法院刑事\\SLDM,87,自,3,20000810.json");
+		ReadJsonVo data = readJson.readJson("C:\\Users\\user\\Desktop\\Jerry\\Java\\臺灣士林地方法院刑事\\SLDM,87,自,241,20000811.json");
 		// 法院 代號
 		String court = data.getId().substring(0, 3);
 		// 案由
 		String charge = data.getTitle();
-		// 刑罰
+		// 刑法
 		String pattern = "刑法第.*條(第.*項)?";
-		Pattern lowPattern = Pattern.compile(pattern);
+		// 刑事訴訟法
+		String pattern2 = "刑事訴訟法第.*條(第.*項)?";
+		Pattern lowPattern = Pattern.compile(pattern2);
 		Matcher matcher = lowPattern.matcher(data.getFull());
 		ArrayList<String> lowList = new ArrayList<>();
 		int index = 0;
 		while(matcher.find(index)) {
-			System.out.println(matcher.group() + " " +matcher.start() + " " + matcher.end());
 			lowList.add(matcher.group());
 			index = matcher.end();
 		}
