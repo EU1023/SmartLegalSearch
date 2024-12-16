3de9a7787f05f6bffcfa27b5bc11c78e862317b2
Rikka Chiu <shigoto312141@gmail.com>
2024-12-16 17:00:03 +0800

    test

---
+++ src/test/java/Test001Test.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Test001Test {
+
+    @Test
+    public void test1() {
+        System.out.println("1");
+    }
+}
